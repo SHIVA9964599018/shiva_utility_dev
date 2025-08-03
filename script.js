fd2b7414d88eaafaad4c09d15e243a56f262c5e5
SHIVA9964599018 <38631570+SHIVA9964599018@users.noreply.github.com>
2025-08-03 15:42:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -973,4 +973,8 @@ window.loadBikeSummary = async function () {
 // Helper: ISO week number
 function getWeekNumber(d) {
   d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
-  const dayNum = d.getUTCDa
+  const dayNum = d.getUTCDay() || 7;
+  d.setUTCDate(d.getUTCDate() + 4 - dayNum);
+  const yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+  return Math.ceil((((d - yearStart) / 86400000) + 1) / 7);
+}
